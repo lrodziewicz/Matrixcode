1f46c09ccfd98681643f0495d31b2ad3af319f03
Łukasz Rodziewicz <lukasz@rodziewicz.com.pl>
2012-05-31 19:54:57 +0200

    Initial commit. Files copied from original project repository at Google Code.

---
+++ Matrixcode/Qrcode/Exception.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to version 1.0 of the Zend Framework
+ * license, that is bundled with this package in the file LICENSE.txt, and
+ * is available through the world-wide-web at the following URL:
+ * http://framework.zend.com/license/new-bsd. If you did not receive
+ * a copy of the Zend Framework license and are unable to obtain it
+ * through the world-wide-web, please send a note to license@zend.com
+ * so we can mail you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Matrixcode
+ * @copyright  Copyright (c) 2009-2011 Peter Minne <peter@inthepocket.mobi>
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+/**
+ * Zend_Exception
+ */
+require_once 'Zend/Exception.php';
+
+/**
+ * Zend_Matrixcode_Exception
+ *
+ * @category   Zend
+ * @package    Zend_Matrixcode
+ * @copyright  Copyright (c) 2009-2011 Peter Minne <peter@inthepocket.mobi>
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class Zend_Matrixcode_Qrcode_Exception extends Zend_Exception
+{
+}
